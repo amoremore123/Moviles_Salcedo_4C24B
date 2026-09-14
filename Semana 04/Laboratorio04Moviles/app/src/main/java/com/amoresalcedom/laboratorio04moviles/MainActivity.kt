0bba9ba414ac429bb3f8403c8f7ab7e1f0069266
amoremore123 <amoresalcmace@gmail.com>
2026-09-14 18:12:42 -0500

    Agregar ejercicio TemperatureDisplay

--- Semana 04/Laboratorio04Moviles/app/src/main/java/com/amoresalcedom/laboratorio04moviles/MainActivity.kt
+++ Semana 04/Laboratorio04Moviles/app/src/main/java/com/amoresalcedom/laboratorio04moviles/MainActivity.kt
@@ -4,13 +4,23 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import com.amoresalcedom.laboratorio04moviles.ui.theme.Laboratorio04MovilesTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,10 +30,7 @@ class MainActivity : ComponentActivity() {
         setContent {
             Laboratorio04MovilesTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    Greeting(
-                        name = "Android",
-                        modifier = Modifier.padding(innerPadding)
-                    )
+                    TemperatureDisplay(modifier = Modifier.padding(innerPadding))
                 }
             }
         }
@@ -31,17 +38,40 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
+fun TemperatureDisplay(modifier: Modifier = Modifier) {
+    var temperature by remember { mutableStateOf(20) }
+
+    Column(
         modifier = modifier
-    )
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Text(
+            text = "Temperatura: $temperature grados",
+            color = when {
+                temperature > 30 -> Color.Red
+                temperature < 10 -> Color.Blue
+                else -> Color.Unspecified
+            }
+        )
+        Button(onClick = { temperature++ }) {
+            Text("Subir")
+        }
+        Button(onClick = { temperature-- }) {
+            Text("Bajar")
+        }
+        Button(onClick = { temperature = 20 }) {
+            Text("Resetear")
+        }
+    }
 }
 
 @Preview(showBackground = true)
 @Composable
 fun GreetingPreview() {
     Laboratorio04MovilesTheme {
-        Greeting("Android")
+        TemperatureDisplay()
     }
 }
