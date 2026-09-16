224ffc5fda24dc0046f0189cc6a18f8a1775c554
Amore Salcedo Macedo <amoresalcmace@gmail.com>
2026-09-16 16:51:02 -0500

    Avance: LazyColumn inicial

--- Semana 04/Lab04Salcedo/app/src/main/java/com/tecsup/lab04salcedo/MainActivity.kt
+++ Semana 04/Lab04Salcedo/app/src/main/java/com/tecsup/lab04salcedo/MainActivity.kt
@@ -10,8 +10,8 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Button
 import androidx.compose.material3.OutlinedTextField
@@ -101,6 +101,16 @@ fun PantallaCarrito(modifier: Modifier = Modifier) {
         }
 
         Text(text = "Productos: ${productos.size}")
-        Spacer(modifier = Modifier.height(8.dp))
+
+        LazyColumn(
+            modifier = Modifier
+                .fillMaxWidth()
+                .weight(1f),
+            verticalArrangement = Arrangement.spacedBy(12.dp)
+        ) {
+            items(productos) { producto ->
+                Text(text = "${producto.nombre} - S/ ${producto.precio}")
+            }
+        }
     }
 }
