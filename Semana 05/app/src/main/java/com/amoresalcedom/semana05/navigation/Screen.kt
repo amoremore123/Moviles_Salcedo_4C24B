1fed9d67e2d42e306dd17fb37a522c58c97b4091
amoremore123 <amoresalcmace@gmail.com>
2026-09-22 22:19:20 -0500

    Añadir ruta de inicio de sesión en la configuración de pantallas

--- Semana 05/app/src/main/java/com/amoresalcedom/semana05/navigation/Screen.kt
+++ Semana 05/app/src/main/java/com/amoresalcedom/semana05/navigation/Screen.kt
@@ -1,10 +1,15 @@
 package com.amoresalcedom.semana05.navigation
 
 sealed class Screen(val route: String) {
+    data object Login : Screen("login")
     data object Home : Screen("home")
-    data object List : Screen("list")
+    data object Reservations : Screen("reservations")
+    data object Routines : Screen("routines")
     data object Profile : Screen("profile")
-    data object Detail : Screen("detail/{itemId}") {
-        fun createRoute(itemId: Int): String = "detail/$itemId"
+    data object Detail : Screen("detail/{classId}") {
+        fun createRoute(classId: Int): String = "detail/$classId"
+    }
+    data object Confirmation : Screen("confirmation/{classId}/{slot}") {
+        fun createRoute(classId: Int, slot: String): String = "confirmation/$classId/$slot"
     }
 }
