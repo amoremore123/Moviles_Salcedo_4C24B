61270ffba0d44b1ee858d954b94e87a8cfa9ecf7
amoremore123 <amoresalcmace@gmail.com>
2026-09-22 22:37:23 -0500

    Ajustar anotacion experimental de Home

--- Semana 05/app/src/main/java/com/amoresalcedom/semana05/ui/HomeScreen.kt
+++ Semana 05/app/src/main/java/com/amoresalcedom/semana05/ui/HomeScreen.kt
@@ -25,6 +25,7 @@ import androidx.compose.material3.BottomAppBar
 import androidx.compose.material3.Button
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.NavigationBarItem
 import androidx.compose.material3.Scaffold
@@ -42,6 +43,7 @@ import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(
     onNavigateToReservations: () -> Unit,
